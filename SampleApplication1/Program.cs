2c2c798e2022bb1f56bb3fe985f2d61fa918d20a
Lorenzo Maiorfi <maiorfi@gmail.com>
2015-06-25 09:46:27 +0200

    Commit iniziale

---
+++ SampleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Questa è la versione 1 dell'applicazione");
+        }
+    }
+}
